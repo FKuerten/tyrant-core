79969d1f8eb7960112826530fdc77dbac423aae2
Fabian <Fabian.Kuerten@rwth-aachen.de>
2014-03-11 00:42:16 +0100

    update to new build system

--- config.mk
+++ config.mk
@@ -1,13 +1,26 @@
+#
+#   Copyright 2014 Fabian "Praetorian" Kürten
+#
+#   This file is part of tyrant-core.
+#   The tyrant-core library is free software: you can redistribute it
+#   and/or modify it under the terms of the GNU General Public License
+#   as published by the Free Software Foundation, exactly version 3 of
+#   the License.
+#
+#   The tyrant-core library is distributed in the hope that it will be
+#   useful but WITHOUT ANY WARRANTY; without even the implied warranty
+#   of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program (COPYING.GPL).  If not, see
+#   <http://www.gnu.org/licenses/>.
+#
+
 # Configuration section
 MODULE_NAME=core
+MODULE_MAJOR_VERSION=1
+MODULE_MINOR_VERSION=0
 DEPENDENCIES=basics
-LIBS:=-lcrypto++
-CXXFLAGS+= --std=c++11
-ARFLAGS:=crvs
-
-# Automatic section
-INCLUDEDIRS:=${MODULEDIR}/target/generated
-iftrue ${DEPENDENCIES}
-	INCLUDEDIRS+=${MODULEDIR}/../${DEPENDENCIES}/src/main/c++
-endif
-CPPFLAGS+=-I${INCLUDEDIRS}
+LIBS:=-lcrypto++ -lpugixml
+CXX_STANDARD=c++11
